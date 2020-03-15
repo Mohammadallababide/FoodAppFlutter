7295f03904eaba7eafbe319b9c75da2da7c9ff84
Mohammadallababide <mohammad999alhos10@gmail.com>
2020-03-15 17:46:37 +0200

    continue in  conect model by scoped model with all the child widget like update and delete product...

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -4,20 +4,39 @@ import '../models/product.dart';
 
 class ProductsModel extends Model {
   List<Product> _products = [];
+  int _selctedProductIndex;
 
   List<Product> get products {
     return List.from(_products);
   }
 
+  int get selctedProductIndex {
+    return _selctedProductIndex;
+  }
+
+  Product get selctedProduct {
+    if (_selctedProductIndex == null) {
+      return null;
+    }
+    return _products[selctedProductIndex];
+  }
+
   void addProduct(Product product) {
     _products.add(product);
+    _selctedProductIndex = null;
   }
 
-  void updateProduct(int index, Product product) {
-    _products[index] = product;
+  void updateProduct(Product product) {
+    _products[_selctedProductIndex] = product;
+    _selctedProductIndex = null;
   }
 
-  void deleteProduct(int index) {
-    _products.removeAt(index);
+  void deleteProduct() {
+    _products.removeAt(_selctedProductIndex);
+    _selctedProductIndex = null;
+  }
+
+  void selectedProduct(int index) {
+    _selctedProductIndex = index;
   }
 }
